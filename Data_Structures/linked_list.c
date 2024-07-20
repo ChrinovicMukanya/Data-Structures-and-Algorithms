70d5cb75abfef1dcb9167554f8d43760d04c1904
Chrinovic マーク <chrinovicmu@gmail.com>
2024-07-21 00:53:24 +0200

    Add files via upload
    Update

--- Data_Structures/linked_list.c
+++ Data_Structures/linked_list.c
@@ -6,24 +6,48 @@ struct Node{
     struct Node *link;
 };
 
-int main(int argc, char **argv)
+void count_nodes(struct Node * head){
+    int count = 0;
+    if(head == NULL){
+        printf("Empty List\n");
+    }
+    struct Node *ptr = NULL;
+    ptr = head;
+
+    while(ptr != NULL){
+        count++;
+        ptr = ptr->link;
+    }
+    printf("%d\n", count);
+}
+void print_data(struct Node *head){
+    if (head == NULL){
+        printf("Empty List\n");
+    }
+    struct Node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL){
+        printf("%d\n", ptr->data);
+        ptr = ptr->link;
+    }
+}
+int main(int argc, char * argv[])
 {
-    struct Node *head = (struct Node*) malloc(sizeof(struct Node));
-    head->data = 10;
+    struct Node * head = (struct Node *) malloc(sizeof(struct Node));
+    head->data = 45;
     head->link = NULL;
 
-    struct Node *current  = (struct Node*) malloc(sizeof(struct Node));
-    current->data = head->data + 10;
+    struct Node * current = (struct Node*) malloc(sizeof(struct Node));
+    current->data = 98;
     current->link = NULL;
     head->link = current;
-
-    current = (struct Node*) malloc(sizeof(struct Node));
-    current->data = head->link->data + 10;
+    
+    current = (struct Node *)malloc(sizeof(struct Node));
+    current->data = 3;
     current->link = NULL;
     head->link->link = current;
-
-    printf("%d---%d---%d\n", head->data, head->link->data, head->link->link->data);
-
-
+    
+    count_nodes(head);
+    print_data(head);
 
 }
