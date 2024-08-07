e9b462f0df0094aecf586e7c292de123b6a4cbe8
Chrinovic マーク <chrinovicmu@gmail.com>
2024-08-07 21:46:33 +0200

    Add files via upload

--- Data_Structures/stack.c
+++ Data_Structures/stack.c
@@ -8,6 +8,12 @@ struct Node{
     struct Node *link;
 }*top = NULL;
 
+int isEmpty(){
+    if (top == NULL){
+        return TRUE;
+    }else 
+        return FALSE;
+}
 void push(int data){
     struct Node *node = (struct Node*) malloc(sizeof(struct Node));
     if (node == NULL){
@@ -22,17 +28,29 @@ void push(int data){
 int pop(){
     struct Node *temp;
     temp = top;
+    if (isEmpty()){
+        printf("Stack underflow");
+        exit(1);
+    }
     int temp_var = temp->data;
     top = top->link;
     free(temp);
     return temp_var;
 }
-void print_data(){
-    if (top == NULL){
-        printf("No stack");
+
+int peek(){
+    if(isEmpty()){
+        printf("Stack underflow");
     }
+    return top->data;
+}
+void print_data(){
     struct Node *ptr;
     ptr = top;
+    if(isEmpty()){
+        printf("Stack underflow");
+        exit(1);
+    }
     int count = 1;
     while (ptr != NULL){
         printf("Node %d = %d\n", count , ptr->data);
@@ -44,7 +62,7 @@ int main(int argc, char ** agrv)
 {
     while(TRUE){
         int option, data;
-        printf("1.Insert\n2.Pop\n3.Show Data\n4.exit\n");
+        printf("1.Insert\n2.Pop\n3.Peek Top\n4.Print Data\n5.Exit\n");
         scanf("%d", &option);
 
         if (option == 1){
@@ -53,9 +71,11 @@ int main(int argc, char ** agrv)
             push(data);
         }else if (option == 2){
             printf("Data popped = %d \n", pop());
-        }else if (option == 3){
-            print_data();
+        }else if (option == 3) {
+            printf("Top element = %d\n", peek());
         }else if (option == 4){
+            print_data();
+        }else if (option == 5){
             printf("Exiting...");
             exit(1);
         }else 
